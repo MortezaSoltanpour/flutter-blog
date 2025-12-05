7b63b422e33ebdcc904ec0be3fdc053d4e6200f7
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-04 23:04:21 -0500

    Change class names

--- lib/constants/myColors.dart
+++ lib/constants/myColors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/widgets.dart';
 
-class solidColors {
+class SolidColors {
   static const Color primaryColor = Color.fromARGB(255, 255, 101, 132);
   static const Color secondaryColor = Color.fromARGB(255, 245, 29, 72);
 }
 
-class gradiantColors {
+class GradiantColors {
   static const List<Color> pinkGradiant = [
     Color.fromARGB(255, 255, 101, 132),
     Color.fromARGB(255, 245, 29, 72),
