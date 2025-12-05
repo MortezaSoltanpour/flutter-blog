d1b7035035d1844148113f24db95081fd3264009
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-04 22:50:38 -0500

    const strings

---
+++ lib/strings.dart
@@ -0,0 +1,5 @@
+class myStrings {
+  static const String welcomeMessage = "Welcome to our application!";
+  static const String loginPrompt = "Please enter your credentials to log in.";
+  static const String errorMessage = "An error has occurred. Please try again.";
+}
