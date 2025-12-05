59a97a4eb96935fa65602b1734987c55a8800384
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-04 23:04:05 -0500

    gradiant Colors

--- lib/constants/myColors.dart
+++ lib/constants/myColors.dart
@@ -1,4 +1,13 @@
+import 'package:flutter/widgets.dart';
+
 class solidColors {
-  static const int primaryColor = 0xFF6C63FF;
-  static const int secondaryColor = 0xFFFF6584;
+  static const Color primaryColor = Color.fromARGB(255, 255, 101, 132);
+  static const Color secondaryColor = Color.fromARGB(255, 245, 29, 72);
+}
+
+class gradiantColors {
+  static const List<Color> pinkGradiant = [
+    Color.fromARGB(255, 255, 101, 132),
+    Color.fromARGB(255, 245, 29, 72),
+  ];
 }
