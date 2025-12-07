b2038b910aa721f7dcf547cd180a169368a30863
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-06 21:03:56 -0500

    TextField

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:blog/gen/assets.gen.dart';
 import 'package:blog/second_screen.dart';
 import 'package:blog/splash_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,27 +12,104 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController instagramIdController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SecondScreen()),
-                    );
-                  },
-                  child: Text("Second page"),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: instagramIdController,
+                  textDirection: TextDirection.ltr,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 0, 133, 18),
+                      ),
+                    ),
+                    labelText: 'Instagram ID',
+                    icon: Icon(Icons.person),
+                    hint: Text('Enter your Instagram ID'),
+                    // helperText: 'This is your unique Instagram identifier',
+                    helper: Row(
+                      children: [
+                        Icon(
+                          CupertinoIcons.person_crop_circle_badge_plus,
+                          size: 20,
+                          color: Colors.blue,
+                        ),
+                        SizedBox(width: 5),
+                        Text('Example: instagram_user123'),
+                      ],
+                    ),
+
+                    prefixText: "@",
+                    suffixText: ".official",
+                    // counterText: "Max 30 characters",
+                    counterStyle: TextStyle(color: Colors.red),
+                    hintStyle: TextStyle(color: Colors.blue),
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                TextField(
+                  textDirection: TextDirection.ltr,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: const Color.fromARGB(255, 0, 133, 18),
+                      ),
+                    ),
+                    labelText: 'Facebook ID',
+                    icon: Icon(Icons.facebook),
+                    hint: Text('Enter your Facebook ID'),
+                    // helperText: 'This is your unique Instagram identifier',
+                    helper: Row(
+                      children: [
+                        Icon(Icons.facebook, size: 20, color: Colors.blue),
+                        SizedBox(width: 5),
+                        Text('Example: /facebook_user123'),
+                      ],
+                    ),
+
+                    prefixText: "@",
+                    suffixText: ".official",
+                    // counterText: "Max 30 characters",
+                    counterStyle: TextStyle(color: Colors.red),
+                    hintStyle: TextStyle(color: Colors.blue),
+                  ),
+                ),
+                Divider(height: 40, thickness: 2),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SecondScreen(
+                            instagramId: instagramIdController.text,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Text("Second page"),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -3,7 +3,9 @@ import 'package:blog/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatefulWidget {
-  const SecondScreen({super.key});
+  final String instagramId;
+
+  const SecondScreen({super.key, required this.instagramId});
 
   @override
   State<SecondScreen> createState() => _SecondScreenState();
@@ -12,23 +14,23 @@ class SecondScreen extends StatefulWidget {
 class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.amber,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Close"),
-                ),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.amber,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(widget.instagramId),
+
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Close"),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
