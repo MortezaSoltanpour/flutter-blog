35c3572f4a6852d68707a390096492363856d3ea
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-06 21:12:16 -0500

    style for text field

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:blog/gen/assets.gen.dart';
 import 'package:blog/second_screen.dart';
-import 'package:blog/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,104 +10,86 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  TextEditingController instagramIdController = TextEditingController();
+  final TextEditingController instagramIdController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  controller: instagramIdController,
-                  textDirection: TextDirection.ltr,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: const Color.fromARGB(255, 0, 133, 18),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: instagramIdController,
+                maxLines: 3,
+                minLines: 1,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: 'Instagram ID',
+                  icon: const Icon(Icons.person),
+                  hintText: 'Enter your Instagram ID',
+                  helper: Row(
+                    children: const [
+                      Icon(
+                        CupertinoIcons.person_crop_circle_badge_plus,
+                        size: 20,
+                        color: Colors.blue,
                       ),
-                    ),
-                    labelText: 'Instagram ID',
-                    icon: Icon(Icons.person),
-                    hint: Text('Enter your Instagram ID'),
-                    // helperText: 'This is your unique Instagram identifier',
-                    helper: Row(
-                      children: [
-                        Icon(
-                          CupertinoIcons.person_crop_circle_badge_plus,
-                          size: 20,
-                          color: Colors.blue,
-                        ),
-                        SizedBox(width: 5),
-                        Text('Example: instagram_user123'),
-                      ],
-                    ),
+                      SizedBox(width: 5),
+                      Text('Example: instagram_user123'),
+                    ],
+                  ),
+                  prefixText: "@",
+                  suffixText: ".official",
 
-                    prefixText: "@",
-                    suffixText: ".official",
-                    // counterText: "Max 30 characters",
-                    counterStyle: TextStyle(color: Colors.red),
-                    hintStyle: TextStyle(color: Colors.blue),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 2),
+                  ),
+                  floatingLabelStyle: TextStyle(
+                    color: Colors.blue, // label color when focused
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(height: 20),
-                TextField(
-                  textDirection: TextDirection.ltr,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: const Color.fromARGB(255, 0, 133, 18),
-                      ),
-                    ),
-                    labelText: 'Facebook ID',
-                    icon: Icon(Icons.facebook),
-                    hint: Text('Enter your Facebook ID'),
-                    // helperText: 'This is your unique Instagram identifier',
-                    helper: Row(
-                      children: [
-                        Icon(Icons.facebook, size: 20, color: Colors.blue),
-                        SizedBox(width: 5),
-                        Text('Example: /facebook_user123'),
-                      ],
-                    ),
+                cursorColor: Colors.red,
+              ),
 
-                    prefixText: "@",
-                    suffixText: ".official",
-                    // counterText: "Max 30 characters",
-                    counterStyle: TextStyle(color: Colors.red),
-                    hintStyle: TextStyle(color: Colors.blue),
+              const SizedBox(height: 20),
+
+              TextField(
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: 'Facebook ID',
+                  icon: const Icon(Icons.facebook),
+                  hintText: 'Enter your Facebook ID',
+                  helper: Row(
+                    children: const [
+                      Icon(Icons.facebook, size: 20, color: Colors.blue),
+                      SizedBox(width: 5),
+                      Text('Example: /facebook_user123'),
+                    ],
                   ),
+                  prefixText: "@",
+                  suffixText: ".official",
                 ),
-                Divider(height: 40, thickness: 2),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SecondScreen(
-                            instagramId: instagramIdController.text,
-                          ),
-                        ),
-                      );
-                    },
-                    child: Text("Second page"),
-                  ),
-                ),
-              ],
-            ),
+              ),
+
+              const Divider(height: 40, thickness: 2),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          SecondScreen(instagramId: instagramIdController.text),
+                    ),
+                  );
+                },
+                child: const Text("Second page"),
+              ),
+            ],
           ),
         ),
       ),
