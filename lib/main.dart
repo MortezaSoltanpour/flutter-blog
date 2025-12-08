be8ff05f2b83a16519d7834f40361583dc11e1a5
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-07 20:29:21 -0500

    Change status bas color

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
-import 'package:blog/gen/assets.gen.dart';
-import 'package:blog/home_screen.dart';
+import 'package:blog/constants/myColors.dart';
 import 'package:blog/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: SolidColors.primaryColor,
+      statusBarIconBrightness: Brightness.light,
+      systemNavigationBarColor: SolidColors.primaryColor,
+      systemNavigationBarIconBrightness: Brightness.light,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
