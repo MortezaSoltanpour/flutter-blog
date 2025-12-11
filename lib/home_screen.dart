375fa3a2a6e2ca0c316e07d29ca8153ad826e80c
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-10 21:11:02 -0500

    Most recent blogs

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:blog/constants/myColors.dart';
 import 'package:blog/gen/assets.gen.dart';
+import 'package:blog/models/model_fake_data.dart';
 import 'package:blog/models/sample_data.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -141,8 +143,90 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
 
               SizedBox(height: 10),
+              Align(
+                alignment: AlignmentGeometry.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 10, 0, 10),
+                  child: Text(
+                    'Most recent blogs',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 160,
 
-              Text('salam'),
+                child: ListView.builder(
+                  itemCount: blogs.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsetsGeometry.fromLTRB(8, 0, 8, 0),
+                      child: Stack(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Image(
+                              height: 160,
+                              fit: BoxFit.cover,
+                              image: AssetImage(blogs[index].image),
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(5),
+                                ),
+                                color: const Color.fromARGB(100, 0, 0, 0),
+                              ),
+                              height: 160,
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 10,
+                            left: 0,
+                            right: 0,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        blogs[index].author,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                      Text(
+                                        blogs[index].view.toString(),
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                  Text(
+                                    blogs[index].title,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
