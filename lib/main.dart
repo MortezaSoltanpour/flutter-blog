90b562cb0c21307287a23570320457f58faadd2c
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-17 23:02:39 -0500

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SplashScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
   }
 }
