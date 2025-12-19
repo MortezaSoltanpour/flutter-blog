e6f51a3bb88f76ad4af272bb12dfcfce699d9f84
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-18 22:06:25 -0500

    Modal bottom sheet

--- lib/pages/modals_screen.dart
+++ lib/pages/modals_screen.dart
@@ -5,8 +5,42 @@ class ModalsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var screen = MediaQuery.of(context).size;
+
     return SafeArea(
-      child: Scaffold(body: Column(children: [Text('This is Modal samples')])),
+      child: Scaffold(
+        appBar: AppBar(title: Text('Modal samples')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print('bottomsheet clicked');
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        padding: const EdgeInsets.all(16),
+                        height: 200,
+                        color: Colors.white,
+                        child: const Center(
+                          child: Text(
+                            'This is modal bottom sheet',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Text('Show bottom sheet'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
