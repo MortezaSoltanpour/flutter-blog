2ac4551bddae2c4ed4d8debf43c249ddba857617
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-18 22:20:19 -0500

    bottomsheet

--- lib/pages/modals_screen.dart
+++ lib/pages/modals_screen.dart
@@ -10,35 +10,63 @@ class ModalsScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: Text('Modal samples')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        body: Builder(
+          builder: (context) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
 
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  print('bottomsheet clicked');
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (context) {
-                      return Container(
-                        padding: const EdgeInsets.all(16),
-                        height: 200,
-                        color: Colors.white,
-                        child: const Center(
-                          child: Text(
-                            'This is modal bottom sheet',
-                            style: TextStyle(fontSize: 18),
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      print('bottomsheet clicked');
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return Container(
+                            padding: const EdgeInsets.all(16),
+                            height: 200,
+                            color: Colors.white,
+                            child: const Center(
+                              child: Text(
+                                'This is modal bottom sheet',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: Text('Show modal bottom sheet'),
+                  ),
+
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('BottomSheet clicked');
+                      Scaffold.of(context).showBottomSheet(
+                        (context) => Container(
+                          height: 200,
+                          width: screen.width,
+                          padding: const EdgeInsets.all(16),
+                          color: Colors.white,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(
+                                context,
+                              ); // closes the modal bottom sheet
+                            },
+                            child: const Text('Close'),
                           ),
                         ),
                       );
                     },
-                  );
-                },
-                child: Text('Show bottom sheet'),
+                    child: const Text('Show bottom sheet'),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
