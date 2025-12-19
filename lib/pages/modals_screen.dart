a5fde9cc47d3631f54d94ea7c42af8829b12991e
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-18 22:24:00 -0500

    Simple dialog

--- lib/pages/modals_screen.dart
+++ lib/pages/modals_screen.dart
@@ -41,28 +41,58 @@ class ModalsScreen extends StatelessWidget {
                   ),
 
                   SizedBox(height: 20),
+
                   ElevatedButton(
                     onPressed: () {
-                      print('BottomSheet clicked');
-                      Scaffold.of(context).showBottomSheet(
-                        (context) => Container(
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Container(
                           height: 200,
-                          width: screen.width,
+                          width: double.infinity,
                           padding: const EdgeInsets.all(16),
                           color: Colors.white,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.pop(
-                                context,
-                              ); // closes the modal bottom sheet
-                            },
-                            child: const Text('Close'),
+                          child: Center(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context); // closes the modal
+                              },
+                              child: const Text('Close'),
+                            ),
                           ),
                         ),
                       );
                     },
                     child: const Text('Show bottom sheet'),
                   ),
+
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Popup Modal'),
+                          content: const Text(
+                            'This is a modal popup. This is a modal popup. This is a modal popup.',
+                          ),
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context); // closes the dialog
+                              },
+                              child: const Text('Close'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: const Text('Show alert dialog'),
+                  ),
+
+                  SizedBox(height: 20),
+                  SizedBox(height: 20),
+                  SizedBox(height: 20),
+                  SizedBox(height: 20),
                 ],
               ),
             );
