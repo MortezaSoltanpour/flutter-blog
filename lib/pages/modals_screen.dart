031148a2ce30b3dc68849318bc8fba9537e5a2aa
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-18 22:54:29 -0500

    All type of modals

--- lib/pages/modals_screen.dart
+++ lib/pages/modals_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ModalsScreen extends StatelessWidget {
   const ModalsScreen({super.key});
 
+  void showCustomToast(BuildContext context, String message) {
+    final overlay = Overlay.of(context);
+    final overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        bottom: 50,
+        left: 20,
+        right: 20,
+        child: Material(
+          color: const Color.fromARGB(137, 0, 0, 0),
+          borderRadius: BorderRadius.circular(8),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              message,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    overlay.insert(overlayEntry);
+    Future.delayed(const Duration(seconds: 2), () => overlayEntry.remove());
+  }
+
   @override
   Widget build(BuildContext context) {
-    var screen = MediaQuery.of(context).size;
-
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: Text('Modal samples')),
@@ -122,21 +147,9 @@ class ModalsScreen extends StatelessWidget {
                     },
                     child: const Text('Show input dialog'),
                   ),
-                  SizedBox(height: 20),
-
-                  ElevatedButton(
-                    onPressed: () {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('This is a Snackbar / Toast'),
-                          duration: Duration(seconds: 2),
-                        ),
-                      );
-                    },
-                    child: const Text('Show Snackbar / Toast'),
-                  ),
 
                   SizedBox(height: 20),
+                  // GeneralDialog
                   ElevatedButton(
                     onPressed: () {
                       showGeneralDialog(
@@ -186,6 +199,50 @@ class ModalsScreen extends StatelessWidget {
                     child: const Text('Show Custom Dialog'),
                   ),
 
+                  SizedBox(height: 20),
+
+                  // Snack bar
+                  ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('This is a Snackbar / Toast'),
+                          duration: Duration(seconds: 4),
+                          elevation: 6,
+                          persist: false,
+                          showCloseIcon: true,
+                        ),
+                      );
+                    },
+                    child: const Text('Show Snackbar / Toast'),
+                  ),
+                  SizedBox(height: 20),
+                  // Flutter toast
+                  ElevatedButton(
+                    onPressed: () {
+                      Fluttertoast.showToast(
+                        msg: "This is a toast",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM, // TOP, CENTER
+                        timeInSecForIosWeb: 1,
+                        backgroundColor: Colors.black54,
+                        textColor: Colors.white,
+                        fontSize: 16.0,
+                      );
+                    },
+                    child: const Text('Show flutter toast'),
+                  ),
+
+                  SizedBox(height: 20),
+                  // Custome toast message
+                  ElevatedButton(
+                    onPressed: () =>
+                        showCustomToast(context, "This is a Custom Toast"),
+                    child: const Text('Show Custom Toast'),
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(height: 20),
+                  SizedBox(height: 20),
                   SizedBox(height: 20),
                 ],
               ),
